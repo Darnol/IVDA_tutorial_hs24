5ce0b92571ac6d8ab24a7f5679bfdc60db99022f
Darnol <32634736+Darnol@users.noreply.github.com>
2024-09-18 14:52:29 +0200

    09: Scatterplot add legend and colors if "All" is selected

--- example-project/hello-world/services/frontend/src/components/ScatterPlot.vue
+++ example-project/hello-world/services/frontend/src/components/ScatterPlot.vue
@@ -18,7 +18,7 @@ export default {
     "selectedCategory"
   ],
   data: () => ({
-    ScatterPlotData: {x: [], y: [], name: []}
+    ScatterPlotData: {x: [], y: [], name: [], cat: []}
   }),
   mounted() {
     this.fetchData()
@@ -27,6 +27,8 @@ export default {
     selectedCategory: function () {
       this.ScatterPlotData.x = [];
       this.ScatterPlotData.y = [];
+      this.ScatterPlotData.name = [];
+      this.ScatterPlotData.cat = [];
       this.fetchData();
     }
   },
@@ -43,23 +45,73 @@ export default {
         this.ScatterPlotData.name.push(company.name)
         this.ScatterPlotData.x.push(company.founding_year)
         this.ScatterPlotData.y.push(company.employees)
+        this.ScatterPlotData.cat.push(company.category)
       })
       // after the data is loaded, draw the plot
       this.drawScatterPlot()
     },
     drawScatterPlot() {
-      var trace1 = {
-        x: this.ScatterPlotData.x,
-        y: this.ScatterPlotData.y,
-        text: this.ScatterPlotData.name,
-        mode: 'markers',
-        type: 'scatter',
-        marker: {
-          color: 'black',
-          size: 12
-        }
-      };
-      var data = [trace1];
+
+      // color cycle for the categories
+      const color_cycle = [
+        "green",
+        "blue",
+        "red",
+        "yellow",
+        "orange",
+        "purple",
+        "brown",
+        "pink",
+        "cyan",
+        "magenta",
+        "grey",
+        "lightblue"
+      ]
+      // Gather all unique categories present in the data
+      let unique_cat = [];
+      unique_cat.push(...new Set(this.ScatterPlotData.cat));
+      // boolean if only one unique category is present (then we dont show colors, but black)
+      let single_cat = unique_cat.length === 1;
+      // map the categories to colors
+      let cat_color_map = {};
+      unique_cat.forEach((cat, index) => {
+        single_cat ? cat_color_map[cat] = 'black' : cat_color_map[cat] = color_cycle[index]
+      })
+      
+
+      // I want to show a legend, so i have to separate the data points into cateogories
+      let traces = [];
+
+      // Create a trace for each unique category
+      unique_cat.forEach((cat, ) => {
+        // For cat, collect all indizes belonging to that cat
+        let category_indices = this.ScatterPlotData.cat
+          .map((c, i) => (c === cat ? i : null))
+          .filter(i => i !== null);
+        
+        // Filter data for the current category
+        let trace_x = category_indices.map(i => this.ScatterPlotData.x[i]);
+        let trace_y = category_indices.map(i => this.ScatterPlotData.y[i]);
+        let trace_text = category_indices.map(i => this.ScatterPlotData.name[i]);
+
+        // get the color of that cat
+        let cat_color = cat_color_map[cat];
+
+        // Create a trace for this category
+        traces.push({
+          x: trace_x,
+          y: trace_y,
+          text: trace_text,
+          mode: 'markers',
+          type: 'scatter',
+          name: cat, // This will be shown in the legend
+          marker: {
+            color: cat_color,
+            size: 12
+          }
+        });
+      });
+
       var layout = {
         xaxis: {
           title: "Founding Year"
@@ -67,9 +119,18 @@ export default {
         yaxis: {
           title: "Number of Employees"
         },
+        showlegend: true, // Show the legend
+        legend: {
+          title: {
+            text: 'Categories'
+          },
+          x: 1,
+          xanchor: 'right',
+          y: 1
+        }
       };
       var config = {responsive: true, displayModeBar: false};
-      Plotly.newPlot('myScatterPlot', data, layout, config);
+      Plotly.newPlot('myScatterPlot', traces, layout, config);
       this.clickScatterPlot();
     },
     clickScatterPlot() {
